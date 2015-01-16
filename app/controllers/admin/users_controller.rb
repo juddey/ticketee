84c59ce261fce1620e91ee05a41e1eff0eaf5d02
Justin Lane <justinlane99@hotmail.com>
2015-01-16 14:43:27 +1300

    Admins can edit project roles of existing users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,7 @@
 class Admin::UsersController < Admin::BaseController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_projects, only: [:new, :create, :edit, :update]
+
   def index
     @users = User.order(:email)
   end
@@ -30,15 +32,24 @@ class Admin::UsersController < Admin::BaseController
        params[:user].delete(:password)
     end
 
-    if @user.update(user_params)
-      flash[:notice] = "User has been updated."
-      redirect_to admin_users_path
-    else
-      flash.now[:alert] = "User has not been updated."
-      render "edit"
+    User.transaction do 
+      @user.roles.clear
+      role_data = params.fetch(:roles, [])
+      role_data.each do |project_id, role_name|
+        if role_name.present?
+          @user.roles.build(project_id: project_id, role: role_name)
+       end
+      end
+
+      if @user.update(user_params)
+        flash[:notice] = "User has been updated."
+        redirect_to admin_users_path
+      else
+        flash.now[:alert] = "User has not been updated."
+        render "edit"
+      end
     end
   end
-
   def destroy
     if @user == current_user
       flash[:alert] = "You cannot delete yourself!"
@@ -59,4 +70,9 @@ class Admin::UsersController < Admin::BaseController
   def set_user
     @user= User.find(params[:id])
   end
+
+  def set_projects
+    @projects = Project.order(:name)
+  end
+
 end
--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -1,2 +1,8 @@
 module Admin::BaseHelper
+
+  def roles
+    hash = {}
+    Role.available_roles.each { |role| hash[role.titleize] = role }
+    hash
+  end
 end
